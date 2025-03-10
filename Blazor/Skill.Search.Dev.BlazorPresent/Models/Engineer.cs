fc7b6a750508363a0c04d1338b8609fd4ac55cdb
Anh Tran <anhtran@Anhs-MacBook-Pro.local>
2025-03-10 13:49:30 +0700

    Blazor UI version 0.2 Search UI improved with dialogs

---
+++ Blazor/Skill.Search.Dev.BlazorPresent/Models/Engineer.cs
@@ -0,0 +1,8 @@
+namespace Skill.Search.Dev.BlazorPresent.Models;
+public class Engineer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Title { get; set; }
+    public List<string> Skills { get; set; }
+}
